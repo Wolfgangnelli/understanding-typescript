7f6e8fc5c6ebc1c853415e06763ea998d13fa963
[AndreaS] <solfa_90@hotmail.it>
2021-06-27 10:11:36 +0200

    Typesctipt basic & Basic Types

--- app.ts
+++ app.ts
@@ -1,61 +1,19 @@
-// CORE TYPES
+// UNKNOWN Type
 
-/* const person: {
-    name: string;
-    age: number;
-    hobbies: string[];
-    activities: any[];
-    role: [number, string];  //tuple types
-} = {
-    name: 'Andrea',
-    age: 30,
-    hobbies: [
-        'Sports',
-        'Cooking'
-    ],
-    activities: [
-        'Run',
-        8,
-        64
-    ],
-    role: [2, 'author'],
-}
-*/
+let userInput: unknown;
+let userName: string;
 
-/* const ADMIN = 0;
-const READ_ONLY = 1;
-const AUTHOR = 2; */
-
-// ENUMS
-enum Role { ADMIN, READ_ONLY, AUTHOR };
-// enum Role { ADMIN = 5, READ_ONLY, AUTHOR };
-// enum Role { ADMIN = 'Admin', READ_ONLY = 10, AUTHOR = 'author' };
-
-
-
-const person = {
-    name: 'Andrea',
-    age: 30,
-    hobbies: [
-        'Sports',
-        'Cooking'
-    ],
-    activities: [
-        'Run',
-        8,
-        64
-    ],
-    role: Role.ADMIN,
+userInput = 5;
+userInput = 'Max';
+if(typeof userInput === 'string') {
+    userName = userInput;
 }
 
-console.log(person);
+// NEVER Type
 
-for (const hobby of person.hobbies) {
-    console.log(hobby.toUpperCase());
+function generateError(message: string, code: number): never {
+    throw {message, errorCode: code};  
 }
 
-if(person.role === Role.ADMIN) {
-    console.log('is admin');
-} else if ( person.role === Role.AUTHOR) {
-    console.log('is author');
-}
+generateError('And error occured!' , 500);
+
