40759ddd7fa7386287b0e25fa0d8fcf7bbb9ffca
[AndreaS] <solfa_90@hotmail.it>
2021-07-05 17:47:56 +0200

    Property, Accessor, Method and Parameter Decorator

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,21 @@
 // META PROGRAMMING
 // Decorators are a functions, i decorators sono eseguiti quando la classe a cui si riferisce viene instanziata e non eseguita/chiamata
 function Logger(logString: string) {
-
-    // Decorator factories, now i can pass parameter to decorator function @Logger(params)
-    return function(_: Function) {
+    console.log('LOGGER FACTORY');
+    // Decorator factories/function, now i can pass parameter to decorator function @Logger(params)
+   /*  return function(_: Function) {
         console.log(logString);
+    } */
+    return function(constructor: Function) {
+        console.log(logString);
+        console.log(constructor);
     }
 }
 
 function WithTemplate(template: string, hookId: string) {
+    console.log('TEMPLATE FACTORY');
     return function(constructor: any) {
+        console.log('Rendering template');
         const hookEm = document.getElementById(hookId);
         const p = new constructor();
         if (hookEm) {
@@ -19,7 +25,7 @@ function WithTemplate(template: string, hookId: string) {
     }
 }
 
-/* @Logger('LOGGING - PERSON') */
+@Logger('LOGGING - PERSON')
 @WithTemplate('<h1>My person Object</h1>', 'app')
 class Person {
     name = 'Max';
@@ -31,3 +37,58 @@ class Person {
 
 const person = new Person();
 console.log(person);
+
+// ---
+
+//Decorator
+function Log(target: any, propertyName: string | Symbol) {
+    console.log('Property decorator!');
+    console.log(target, propertyName);
+}
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+    console.log('Accessor decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+
+function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
+    console.log('Method decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+
+function Log4(target: any, name: string | Symbol, position: number) {
+    console.log('Parameter decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(position); // index del parametro
+}
+
+class Product {
+    @Log
+    title: string;
+    private _price: number;
+
+    // Add Decorator a un accessor
+    @Log2
+    set price(val: number) {   // Accessor
+        if(val > 0) {
+            this._price = val;
+        } else {
+            throw new Error('Invalid price, should be positive!');
+        }
+    }
+
+    constructor(t: string, p: number) {
+        this.title = t;
+        this._price = p;
+    }
+    // Add Decorator a un method
+    @Log3
+    getPriceWithTax(@Log4 tax: number) {
+        return this._price * (1+tax);
+    }
+}
